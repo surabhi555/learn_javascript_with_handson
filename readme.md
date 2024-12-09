45439b5679dd575121f5974ed1b9acd3971f94c2
surabhi555 <pednekarsurabhi555@gmail.com>
2024-12-09 20:06:08 +0530

    stack and heap area

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 #learning js concepts via simple handson programs
+1.stack and heap area 
---
+++ stackAndHeap.js
@@ -0,0 +1,19 @@
+let myName="surabhi";
+let anotherName=myName;
+
+myName="kiran";
+console.log(anotherName); //output: surabhi
+console.log(myName);
+
+let userOne={
+    email:"one@gmail.com",
+    age:24
+}
+let userTwo=userOne
+
+userTwo.email="kiran@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
